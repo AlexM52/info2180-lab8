82177fa6fb8c2318b7ebc8feae1f828d19c764d4
alexm52 <alexmcdoom@hotmail.com>
2014-11-22 02:47:38 +0000

    Exercise 3 complete: response now displays in 'result' div in world.html

--- world.js
+++ world.js
@@ -8,19 +8,21 @@ function lookupClick(){
     new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?lookup="+$$('#term')[0].value,
     {
         method: "get",
-        onSuccess: alertInfo,
+        onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
         onFailure: ajaxFailure,
         onException: ajaxFailure
     }
 );
 }
 
+// Display response content in an alert dialog box
 function alertInfo(ajax){
 window.alert(ajax.responseText);
 }
 
+// Inserts appropriate response content into 'results' div
 function updateInfo(ajax){
-//insert text...innerhtml??
+$$("#result")[0].innerHTML = ajax.responseText;
 }
 
 function ajaxFailure(ajax, exception){
