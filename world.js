c8d894fbae98dbcc32c1554c663d8c5a38acfd37
alexm52 <alexmcdoom@hotmail.com>
2014-11-22 05:02:22 +0000

    Exercise 4 completed: world.php?all=true behaves as specified.

--- world.js
+++ world.js
@@ -5,24 +5,63 @@ window.onload = function(){
 function lookupClick(){
     //window.alert("test");
     //new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?lookup=Jamaica",
-    new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?lookup="+$$('#term')[0].value,
-    {
-        method: "get",
-        onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
-        onFailure: ajaxFailure,
-        onException: ajaxFailure
+    var term = $$('#term')[0].value;
+    var all = $$("#all")[0];
+    //console.log(all.checked);
+    var requestString = "https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php";
+    var optCnt = 0;
+    if ((term !== '') || (all.checked===true)){
+        requestString += "?";
     }
-);
+    if (term !== ''){
+        requestString += "lookup="+term;
+        optCnt++;
+    }
+    if (all.checked===true){
+        if(optCnt>0){
+            requestString += "&";
+        }
+        requestString += "all="+all.checked;
+        optCnt++;
+    }
+    /*console.log(term!=="");
+    if(term !== ""){
+        new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?lookup="+term,
+        {
+            method: "get",
+            onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
+            onFailure: ajaxFailure,
+            onException: ajaxFailure
+        }
+        );
+    }else{
+        new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?all="+all.checked,
+        {
+            method: "get",
+            onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
+            onFailure: ajaxFailure,
+            onException: ajaxFailure
+        }
+        );
+    }*/
+    new Ajax.Request(requestString,
+        {
+            method: "get",
+            onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
+            onFailure: ajaxFailure,
+            onException: ajaxFailure
+        }
+        );
 }
 
 // Display response content in an alert dialog box
 function alertInfo(ajax){
-window.alert(ajax.responseText);
+window.alert(ajax.responseXML);
 }
 
 // Inserts appropriate response content into 'results' div
 function updateInfo(ajax){
-$$("#result")[0].innerHTML = ajax.responseText;
+    $$("#result")[0].innerHTML = ajax.responseText;
 }
 
 function ajaxFailure(ajax, exception){
--- world.php
+++ world.php
@@ -7,14 +7,38 @@ mysql_select_db("world");
 
 
 $LOOKUP = $_REQUEST['lookup'];
+$ALL = $_REQUEST['all'];
 
 # execute a SQL query on the database
-$results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
-print $results;
-# loop through each country
-while ($row = mysql_fetch_array($results)) {
-  ?>
-  <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
-  <?php
+if(sizeof($LOOKUP)==0){
+  if($ALL == 'true'){
+    $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
+    print $results;
+    # loop through each country
+    
+    while ($row = mysql_fetch_array($results)) {
+      ?>
+      <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
+      <?php
+    }
+  }else{
+    $results = msql_query("SELECT * FROM countries WHERE name LIKE '$LOOKUP';");
+    print $results;
+    while ($row = mysql_fetch_array($results)){
+      ?>
+      <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
+      <?php
+    }
+  }
+}else{
+  $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
+  print $results;
+  # loop through each country
+  
+  while ($row = mysql_fetch_array($results)) {
+    ?>
+    <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
+    <?php
+  }
 }
 ?>
