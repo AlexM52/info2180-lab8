306a9734798771d9e56fc6d3baee7538800dc3b0
alexm52 <alexmcdoom@hotmail.com>
2014-11-22 07:25:42 +0000

    Code cleaned and commented. Exercise 5 complete as in last commit.

--- world.js
+++ world.js
@@ -1,56 +1,48 @@
+// On load, set button handler
 window.onload = function(){
     $$('#lookup')[0].onclick = lookupClick;
 };
 
+// Some variables
+var term;       // The search term (Country name)
+var all;        // The all checkbox
+var formatXML;  // The XML format checkbox
+
+// Lookup button handler
 function lookupClick(){
-    //window.alert("test");
-    //new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?lookup=Jamaica",
-    var term = $$('#term')[0].value;
-    var all = $$("#all")[0];
-    var formatXML = $$("#format")[0];
-    //console.log(all.checked);
+    // Set variables
+    term = $$('#term')[0].value;
+    all = $$("#all")[0];
+    formatXML = $$("#format")[0];
+    
+    // Set base request string for AJAX
     var requestString = "https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php";
     var optCnt = 0;
+    
+    // If there are any parameters, prepare to add them to string above
     if ((term !== '') || (all.checked===true || (formatXML.checked===true))){
         requestString += "?";
     }
-    if (term !== ''){
+    
+    if (term !== ''){                               // Adds lookup term
         requestString += "lookup="+term;
         optCnt++;
     }
-    if (all.checked===true){
+    if (all.checked===true){                        // Adds all=true
         if(optCnt>0){
             requestString += "&";
         }
         requestString += "all="+all.checked;
         optCnt++;
     }
-    if (formatXML.checked===true){
+    if (formatXML.checked===true){                  // Adds format=xml
         if(optCnt>0){
             requestString += "&";
         }
         requestString += "format="+formatXML.value;
     }
-    /*console.log(term!=="");
-    if(term !== ""){
-        new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?lookup="+term,
-        {
-            method: "get",
-            onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
-            onFailure: ajaxFailure,
-            onException: ajaxFailure
-        }
-        );
-    }else{
-        new Ajax.Request("https://2180dev-alexm52.c9.io/webgit/AlexM52.github.io/info2180lab8/world.php?all="+all.checked,
-        {
-            method: "get",
-            onSuccess: updateInfo,          // NOTE: can switch to using alert box display by replacing with 'alertInfo' function.
-            onFailure: ajaxFailure,
-            onException: ajaxFailure
-        }
-        );
-    }*/
+    
+    // Create AJAX request object
     new Ajax.Request(requestString,
         {
             method: "get",
@@ -62,13 +54,17 @@ function lookupClick(){
 }
 
 // Display response content in an alert dialog box
+// ( Now modified to behave similarly to updateInfo() )
 function alertInfo(ajax){
-window.alert(ajax.responseXML);
+if(formatXML.checked){
+        window.alert(ajax.responseXML);
+    }else{
+        window.alert(ajax.responseText);
+    }
 }
 
 // Inserts appropriate response content into 'results' div
 function updateInfo(ajax){
-    var formatXML = $$("#format")[0];
     if(formatXML.checked){
         $$("#result")[0].innerHTML = ajax.responseXML;
     }else{
@@ -76,6 +72,7 @@ function updateInfo(ajax){
     }
 }
 
+// In case ajax request doesn't work.
 function ajaxFailure(ajax, exception){
 alert("Ajax request error: \n\nServer status: " + ajax.status + " " + ajax.statusText +
 "\n\nServer Response: " + ajax.responseText);
--- world.php
+++ world.php
@@ -1,21 +1,28 @@
 <?php
+#Connect to db
 mysql_connect(
 "0.0.0.0",
 "alexm52"
 );
 mysql_select_db("world");
 
-
+#parameters
 $LOOKUP = $_REQUEST['lookup'];
 $ALL = $_REQUEST['all'];
 $FORMAT = $_REQUEST['format'];
 
 # execute a SQL query on the database
+#If no lookup term given...
 if(sizeof($LOOKUP)==0){
+  #and 'all' was checked...
   if($ALL == 'true'){
+    #query the db
     $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
     
+    #if 'xml' was checked
     if($FORMAT == 'xml'){
+      #output XML file
+      #XML OUTPUT CODE-------------------------------------------------------
       header('Content-Type: text/xml');
       echo '<?xml version="1.0" encoding="UTF-16"?>'; ?>
 <countrydata><?php
@@ -29,7 +36,10 @@ if(sizeof($LOOKUP)==0){
       } ?>
 </countrydata>
         <?php
+        #-------------------------------------------------------------------
+        
     }else{
+      #otherwise, generate html response...
       print $results;
       # loop through each country
       
@@ -39,14 +49,21 @@ if(sizeof($LOOKUP)==0){
         <?php
       }
     }
+    
+  #if 'all' was not checked...
   }else{
     // Effectively output nothing, but so client side still receives a response:
     ?>
       
       <?php
   }
+  
+#if a lookup term was given...  
 }else{
+  #and 'xml' was checked...
   if($FORMAT == 'xml'){
+    #output XML
+    #XML OUTPUT CODE---------------------------------------------------------
       header('Content-Type: text/xml');
       echo '<?xml version="1.0" encoding="UTF-16"?>'; ?>
 <countrydata><?php
@@ -60,6 +77,9 @@ if(sizeof($LOOKUP)==0){
       } ?>
 </countrydata>
         <?php
+        #-------------------------------------------------------------------
+        
+  #otherwise, generate html
   }else{
     $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
     print $results;
